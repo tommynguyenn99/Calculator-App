dcfe58cfb80e624bc93b27c20fb4cc5773a1576f
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-04-19 13:13:07 -0700

    starter-calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import Header from "./Header";
+import { calculatorButtons } from "./calculator-base-button-data.js";
+import Footer from "./Footer";
+
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      {calculatorButtons.map((button) => (
+        <button key= {button.value}>{button.text}</button>
+      ))}
+    </div>
+  );
+}
+
+
+
 
 function App() {
+
+// Handle Click 
+const variables = { 
+    title: 'React Calculator', 
+    copyright: 2023, 
+    creator: 'Tommy, Brooke, Zybiah'
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <Header className="calculator-header" title={variables.title}/>
+      <Calculator/>
+      {/* Footer */}
+      <Footer className="footer" copyright={variables.copyright} creator={variables.creator}/>
     </div>
   );
 }
